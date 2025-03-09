fdeeb453b5739a868064b659b83be6e600c53da2
Wahid Ali <wahidali8566@gmail.com>
2025-03-09 06:47:15 +0530

    dsa

--- Arrays2/3SumClosest.cpp
+++ Arrays2/3SumClosest.cpp
@@ -11,25 +11,19 @@ int threeSumClosest(vector<int> &v, int target) {
 
     sort(v.begin(), v.end());
 
-    for(int i =0; i<n-2; i++) {
-
-        if(1 > 0 && v[i] == v[i-1]) continue; 
-
+    for(int i=0; i<n-2; i++) {
         int j = i+1;
         int k = n-1;
 
         while(j<k) {
             int sum = v[i] + v[j] + v[k];
-            
-            if(sum < target) j++;
-            else if(sum > target) k--;
 
-            else if(sum == target) return target;
-            else if(abs(sum - target) < abs(resultSum - target)) {
-                resultSum = sum;
-            }
+            if(sum == target) return target;
+            if(abs(sum-target) < abs(resultSum-target)) 
+            resultSum = sum;
 
-
+            if(sum < 0) j++;
+            else k--;
         }
     }
     return resultSum;
@@ -46,7 +40,10 @@ int main() {
     v.push_back(0);
     v.push_back(-4);
     v.push_back(-5);
-
+    // v.push_back(0);
+    // v.push_back(0);
+    // v.push_back(0);
+    
     int target = 1;
 
     for(int i=0; i<v.size(); i++) {
