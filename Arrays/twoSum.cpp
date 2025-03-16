f90d77492827214aaecc69f3fa5079fc9ddf230e
Wahid Ali <wahidali8566@gmail.com>
2025-03-16 22:02:51 +0530

    dsa

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -20,12 +20,38 @@ int main() {
     cout<<"Enter the target: ";
     cin>>x;
 
-    for(int i=0; i<=v.size() -1 ; i++) {
-        for(int j=i+1; j<=v.size() -1; j++) {
-            if(v[i]+v[j] == x) {
-                cout<<"("<<i<<","<<j<<")"<<endl;
-            }
+    // for(int i=0; i<=v.size() -1 ; i++) {
+    //     for(int j=i+1; j<=v.size() -1; j++) {
+    //         if(v[i]+v[j] == x) {
+    //             cout<<"("<<i<<","<<j<<")"<<endl;
+    //         }
+    //     }
+    // }
+    int left = 0;
+    int right = n-1;
+
+    bool found = false;
+
+    while (left <= right) {
+        int sum = v[left] + v[right];
+
+        if (sum == x) {
+            cout << "Target found at"<<left<<"and"<<right<<endl;
+            found = true;
+            break;
+        }
+
+        if (sum < x) {
+            left++;
+        } else {
+            right--;  
         }
     }
+
+    if (!found) {
+        cout<<"Target not found"<< endl;
+    }
+
+    return 0;
 
 }
