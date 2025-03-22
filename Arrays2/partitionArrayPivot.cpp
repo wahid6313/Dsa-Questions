0daac3a97c567003fe47cc73075d6a4f567d2157
Wahid Ali <wahidali8566@gmail.com>
2025-03-22 11:10:28 +0530

    dsa

--- Arrays2/partitionArrayPivot.cpp
+++ Arrays2/partitionArrayPivot.cpp
@@ -42,9 +42,9 @@ int main() {
     v.push_back(4);
     v.push_back(3);
     v.push_back(2);
-    // v.push_back(14);
-    // v.push_back(3);
-    // v.push_back(10);
+    v.push_back(14);
+    v.push_back(3);
+    v.push_back(10);
 
     for(int i=0; i<=v.size(); i++) {
         cout<<v[i]<<" ";
