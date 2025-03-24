6c74955fe4e23885a0450bbd33a44e4fd8cc99f2
Wahid Ali <wahidali8566@gmail.com>
2025-03-24 09:27:33 +0530

    dsa

---
+++ linearSearch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter the number: ";
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    
+    int x;
+    cout<<"enter target ";
+    cin>>x;
+    
+    int flag = false;
+    for(int i=0; i<=n-1; i++) {
+        if(arr[i] == x) flag = true;
+    }
+    if(flag == true) cout<<"element found";
+    else cout<<"element not found";
+
+    
+
+    
+
+}
