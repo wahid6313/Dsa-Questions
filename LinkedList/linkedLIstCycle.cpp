b5546314d8f83c61ae88892468eec771e6b2ca5e
Wahid Ali <wahidali8566@gmail.com>
2025-04-07 15:08:44 +0530

    dsa

---
+++ LinkedList/linkedLIstCycle.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+
+        Node(int val) {
+            this->val = val;
+            this->next = NULL;
+        }
+};
+void display(Node* head) {
+    while(head != NULL) {
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+bool linkedListCycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return true;
+    }
+    return false;
+}
+
+
+int main() {
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    Node* e = new Node(50);
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+
+    e->next = c;
+
+    if(linkedListCycle(a)) {
+        cout<<"Cycle detected."<<endl;
+    }
+    else{
+        cout<<"No cycle."<<endl;
+        display(a);
+    }
+    
+
+    
+    
+}
