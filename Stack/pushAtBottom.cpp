37e22f286d442f115c829e2b6e186c97594eece7
Wahid Ali <wahidali8566@gmail.com>
2025-04-19 12:04:59 +0530

    dsa

--- Stack/pushAtBottom.cpp
+++ Stack/pushAtBottom.cpp
@@ -2,6 +2,20 @@
 #include <stack>
 using namespace std;
 
+void print(stack<int> &st) {
+    stack<int> temp;
+    while(st.size() > 0) {
+        temp.push(st.top());
+        st.pop();
+
+    }
+    while(temp.size() > 0) {
+        cout<<temp.top()<<" ";
+        st.push(temp.top());
+        temp.pop();
+    }
+}
+
 int main() {
     stack<int> st;
     
@@ -11,18 +25,5 @@ int main() {
     st.push(40);
     st.push(50);
 
-    stack<int> temp;
-    while(st.size() > 0) {
-        cout<<st.top()<<" ";
-        temp.push(st.top());
-        st.pop();
-    }
-    while(temp.size() > 0) {
-       
-        st.push(temp.top());
-        temp.pop();
-    }
-    cout<<endl<<st.top();
-    
-
+    print(st);
 }
