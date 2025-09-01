794884fd17363af2ad872245b8bd2437321c3946
Wahid Ali <wahidali8566@gmail.com>
2025-09-01 14:13:04 +0530

    stack

--- Stack/balancedBracket.cpp
+++ Stack/balancedBracket.cpp
@@ -3,23 +3,26 @@
 using namespace std;
 
 bool isBalanced(string s) {
-    if(s.length() %2 != 0)
+    if(s.length() % 2 != 0)
         return false;
+
     stack<char> st;
-    for (int i = 0; i < s.length(); i++ ) {
-        if(s[i] == '(') st.push(s[i]);
-        else {
+    for (int i = 0; i < s.length(); i++) {
+        if(s[i] == '(')
+            st.push(s[i]);
+        else
             if(st.size() == 0) return false;
             else
                 st.pop();
-        }
     }
-    if(st.size() == 0) return true;
+    if(st.size() == 0)
+        return true;
     else
         return false;
 }
 
 int main() {
-    string s = ")()()()()";
+    string s = "()()()()";
     cout << isBalanced(s) <<  endl;
 }
+
--- Stack/stockSpan.cpp
+++ Stack/stockSpan.cpp
@@ -12,27 +12,28 @@ int main() {
     cout<<endl;
 
     int pgi[n];
-    stack<int> st;
 
+    stack<int> st;
     pgi[0] = 1;
     st.push(0);
 
-    for(int i = 1; i<= n-1; i++) {
+    for (int i = 1; i < n; i++) {
         while(st.size() > 0 && arr[st.top()] < arr[i]) {
             st.pop();
         }
-        if(st.size() == 0) pgi[i] = -1;
-        else pgi[i] = st.top();
-
-        pgi[i] = i-pgi[i];
-
+        if(st.size() == 0)
+            pgi[i] = -1;
+        else
+            pgi[i] = st.top();
+        pgi[i] = i - pgi[i];
         st.push(i);
     }
 
-    for(int i =0; i<n; i++) {
-        cout<<pgi[i]<<" ";
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << pgi[i] << " ";
     }
-
-
-
+        cout << endl;
 }
